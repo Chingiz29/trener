b1801fe907cdcfaf7b2667d2412a5665d02494bf
Chingiz29 <hantimirovcingiz@gmail.com>
2026-08-06 16:56:43 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+var C = 'trener-v1';
+var FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(C).then(function(c){ return c.addAll(FILES); }).then(function(){ return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(k){
+    return Promise.all(k.filter(function(x){ return x !== C; }).map(function(x){ return caches.delete(x); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+self.addEventListener('fetch', function(e){
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      return r || fetch(e.request).then(function(res){
+        var cp = res.clone();
+        caches.open(C).then(function(c){ c.put(e.request, cp); });
+        return res;
+      }).catch(function(){ return caches.match('./index.html'); });
+    })
+  );
+});
